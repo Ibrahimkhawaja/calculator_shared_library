d8dfbf436e2cec023cc23221ef091252fda32d02
Ibrahimkhawaja <khawajaibrahim2011@hotmail.com>
2020-10-19 15:17:50 +0500

    Update mypipeline.groovy

--- vars/mypipeline.groovy
+++ vars/mypipeline.groovy
@@ -2,62 +2,33 @@ def call(body) {
     pipeline {
     agent any
     
-    environment {
-        // This can be nexus3 or nexus2
-        NEXUS_VERSION = "nexus3"
-        // This can be http or https
-        NEXUS_PROTOCOL = "https"
-        // Where your Nexus is running
-        NEXUS_URL = "nexus.mynisum.com:8443/"
-        // Repository where we will upload the artifact
-        NEXUS_REPOSITORY = "maven-snapshots"
-        // Jenkins credential id to authenticate to Nexus OSS
-        NEXUS_CREDENTIAL_ID = "nexus-registry-cred"
-        // Build Dir where Build will be executed
-        BUILD_DIRECTORY = "ms-config-server/"
-		// Kubernetes namespace
-		KUBERNETES_NAMESPACE = "development"
-		// Kubeconfig path
-		KUBECONFIG_PATH = "~/.kube/config"
-		// Helm charts branch for development and qabgn
-		HELM_CHARTS_BRANCH = "devops"
-     }
+    //environment { 
+     //}
      stages {
-        stage('Initialize env') {
+        stage('Add') {
             steps {
                 script {
-                    if (env.JOB_NAME == 'msconfig-test') {
-                        env.BUILD_VERSION = "test-version"
-                        env.JOB_BUILD = 'msconfig'
+                    if (env.JOB_NAME == 'projectA') {
+                        add()
                     }
                 }
             }
         } 
-        stage('Build and Publish image') {
+        stage('Subtract') {
             steps {
-                sh 'echo build step'
+                subtract()
             }
         }
-        stage('Upload Artifact') {
+        stage('test') {
             steps {
                 script {
-                    if (env.GIT_BRANCH == 'origin/release') {
-                        NEXUS_REPOSITORY = "maven-releases"
-						KUBERNETES_NAMESPACE = "qabgn"
-						KUBECONFIG_PATH = "~/.kube/config.qa"
-						HELM_CHARTS_BRANCH = "qabgn"
+                    if (env.GIT_BRANCH == 'origin/main') {
+                        sh 'hello'
                     }
-                }
-                
+		}
             }
         }
-        stage('Deploy') {
-            steps {
-                sh 'echo NEXUS-REPOSITORY = ${NEXUS_REPOSITORY} '
-                sh 'echo BUILD-VERSION = ${BUILD_VERSION}'
-                sh 'echo JOB-BUILD = ${JOB_BUILD}'
-            }
-        }
+        
     }
 }
 }
