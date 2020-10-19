ff4fdf95d70233d77d237d3be1f34a00b5fad587
Ibrahimkhawaja <khawajaibrahim2011@hotmail.com>
2020-10-19 15:25:04 +0500

    Update multiply.groovy

--- vars/multiply.groovy
+++ vars/multiply.groovy
@@ -3,7 +3,7 @@ def call(body) {
     def res = new Multiplier().Multiply(4,2)
 
     echo "$res"
-    currentBuild.result = 'SUCCESS' //FAILURE to fail
+    //currentBuild.result = 'SUCCESS' //FAILURE to fail
 
     return this
 }
