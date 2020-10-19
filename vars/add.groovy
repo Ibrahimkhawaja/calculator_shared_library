47b111fe6b81e58a39a56210b1b3bb0c693a97a3
Ibrahimkhawaja <khawajaibrahim2011@hotmail.com>
2020-10-19 15:24:12 +0500

    Update add.groovy

--- vars/add.groovy
+++ vars/add.groovy
@@ -3,7 +3,7 @@ def call(body) {
     def res = new Adder().Add(4,2)
 
     println "$res"
-    currentBuild.result = 'SUCCESS' //FAILURE to fail
+    //currentBuild.result = 'SUCCESS' //FAILURE to fail
 
     return this
 }
